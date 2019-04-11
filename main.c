de1a2f76e47a1ab371d9b3603c9e2635619f0561
Tommy Clare <tclare@nd.edu>
2019-04-10 21:26:03 -0400

    you're a dingus

--- main.c
+++ main.c
@@ -22,42 +22,49 @@ char *repAlg = NULL;
 int *physicalMemoryUsed = NULL;
 struct disk * d = NULL;
 
-void randAlg(struct page_table*, int);
-void fifo(struct page_table*, int);
-void custom(struct page_table*, int);
+int nPageFaults = 0;
+int nDiskReads = 0;
+int nDiskWrites = 0;
+
+
+typedef struct Node {
+	int frame;
+	struct Node * next;
+} frameNode;
+
+frameNode * f = NULL;
+
+void replacementAlg(struct page_table*, int);
+int randomAlg(struct page_table*);
+void customAlg(struct page_table*, int);
 int firstEmpty(struct page_table*);
 void clear_bits_by_frame(struct page_table*, int);
-
+int get_page_by_frame(struct page_table *, int);
+void pushNode(int);
+int popNode();
 
 void page_fault_handler( struct page_table *pt, int page)
 {
-	printf("page fault on page #%d\n",page);
+	printf("\n\npage fault on page #%d\n\n",page);
+	nPageFaults++;
 
 	// Check if it's already in the page table and has read bit set.
 
 	int frame, bits;
 
+	printf("\n\n\n");
+
 	page_table_get_entry(pt,page,&frame,&bits);
-	if (bits & PROT_READ) {
+	if (bits & PROT_READ) {	
 		bits = bits | PROT_WRITE;
 		page_table_set_entry(pt, page, frame, bits);
+		page_table_print(pt);	
 		return;
 	}
 
-	if (STREQ(repAlg,"rand")) {
-		randAlg(pt, page);
-	}
-	else if (STREQ(repAlg, "fifo")) {
-		fifo(pt, page);
-	}
-	else if (STREQ(repAlg, "custom")) {
-		custom(pt, page);
-	}
-	else {
-		fprintf(stderr, "main.c: invalid replacement algorithm\n");
-		exit(1);
-	}
-	//exit(1);
+	replacementAlg(pt,page);
+
+	page_table_print(pt);	
 	return;
 }
 
@@ -78,7 +85,17 @@ int main( int argc, char *argv[] )
 		exit(1);
 	}
 	strcpy(repAlg, argv[3]);
+
+	if (!STREQ(repAlg,"fifo") && !STREQ(repAlg, "rand") && !STREQ(repAlg,"custom")) {
+		fprintf(stderr, "main.c: invalid replacement algorithm\n");
+		exit(1);
+	}	
+
 	const char *program = argv[4];
+
+	f = malloc(sizeof(frameNode)); // mallocs the starting Node for the head of the list 
+	f->next = NULL;
+	f->frame = -1;
 
 	struct disk *disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
@@ -115,56 +132,100 @@ int main( int argc, char *argv[] )
 	}
 
 	free(repAlg);
+	frameNode * fr = f->next;
+	while (fr) {
+		 free(f);
+		 f = fr;
+		 fr = fr->next;
+	}
+	free(f);
+
 	free(physicalMemoryUsed);
 	page_table_delete(pt);
 	disk_close(disk);
 
+	printf("# page faults: %d;  # disk reads: %d;  # disk writes: %d\n",nPageFaults,nDiskReads,nDiskWrites);
+
 	return 0;
 }
 
-void randAlg(struct page_table* pt, int page) {
-	int nframes = page_table_get_nframes(pt);
-	int randFrameNum;
+void pushNode(int frameNo) {
+	if (f->frame == -1) {
+		f->frame = frameNo;	
+		return;
+	}
+	frameNode * curr = f;
+	while (curr->next) {
+		curr=curr->next;
+	}
+	curr->next = malloc(sizeof(frameNode));
+	curr->next->frame = frameNo;
+	curr->next->next = NULL;	
+}
+
+int popNode() {
+	int returnVal = f->frame;
+	if (f->next) f = f->next;
+	else f->frame = -1;
+	return returnVal;
+}
+
+
+void replacementAlg(struct page_table* pt, int page) {
+	int frameToReplace;
 	int frame, bits;
+	int pageToGet;
 	int emptyFrame = firstEmpty(pt);
 	char buffer[BLOCK_SIZE];
-	if (emptyFrame < 0) { // no empty page table entries
-		randFrameNum = rand()%nframes; // random page to evict.
-		page_table_get_entry(pt,randFrameNum,&frame,&bits);
-		physicalMemoryUsed[randFrameNum] = 0;
+
+	if (emptyFrame < 0) { // no empty page table entries		
+		 // random frame to evict.	
+		if (STREQ(repAlg,"rand")) frameToReplace = randomAlg(pt);
+		else if (STREQ(repAlg, "fifo")) frameToReplace = popNode();
+		pageToGet = get_page_by_frame(pt, frameToReplace);
+		page_table_get_entry(pt,pageToGet,&frame,&bits);
+		physicalMemoryUsed[frameToReplace] = 0;
 		if (bits & PROT_WRITE) {
 			// write page to disk
-			disk_write(d,randFrameNum,page_table_get_physmem(pt)+PAGE_SIZE*randFrameNum);	
+			disk_write(d,frameToReplace,page_table_get_physmem(pt)+PAGE_SIZE*frameToReplace);
+			nDiskWrites++;	
 		}
-		emptyFrame = randFrameNum;
+		emptyFrame = frameToReplace;
 		// clear bits on the evicted frame in page table
-		clear_bits_by_frame(pt, randFrameNum);
+		clear_bits_by_frame(pt, frameToReplace);
 	}
+
+	if (STREQ(repAlg,"fifo")) pushNode(emptyFrame);
+		
 	// write to empty frame from disk
 	disk_read(d,emptyFrame,buffer);
+	nDiskReads++;
 	void * pm = page_table_get_physmem(pt);
 	void * memdest = memcpy(pm + PAGE_SIZE*emptyFrame, buffer,PAGE_SIZE);
 	// set corresponding page table entry
-	page_table_set_entry(pt,page,emptyFrame,PROT_READ);	 
-
+	page_table_set_entry(pt,page,emptyFrame,PROT_READ);
+	physicalMemoryUsed[emptyFrame] = 1;	 
 	return;
 }
 
-void fifo(struct page_table* pt, int page) {
-	return;
+int randomAlg(struct page_table * pt) {
+	int nframes = page_table_get_nframes(pt);
+	return rand() % nframes; 
 }
 
-void custom(struct page_table* pt, int page) {
+
+void customAlg(struct page_table* pt, int page) {
 	return;
 }
 
 // return number of first empty frame in physical memory
 int firstEmpty(struct page_table *pt) {
-	// iterate through by PAGE_SIZE and find first empty page
+	// iterate through and find first empty frame
 	int i;
 	for (i = 0; i < page_table_get_nframes(pt); i++) {
 		if (!physicalMemoryUsed[i])  {
-			physicalMemoryUsed[i] = 1;
+			//physicalMemoryUsed[i] = 1;
+			printf("empty frame found: %d\n",i);
 			return i;
 		}
 	}
@@ -177,10 +238,24 @@ void clear_bits_by_frame(struct page_table *pt, int frame) {
 	int page;
 	int currFrame;
 	int bits;
-	for (page = 0; page < page_table_get_nframes(pt); page++) {
+	for (page = 0; page < page_table_get_npages(pt); page++) {
 		page_table_get_entry(pt, page, &currFrame, &bits);
-		if (currFrame == frame) {
-			page_table_set_entry(pt, page, frame, 0); // set bits to 0
+		if (currFrame == frame && bits) {
+			page_table_set_entry(pt, page, 0, 0); // set bits to 0
 		}
 	}
+}
+
+// return a page that's currently mapped to frame
+int get_page_by_frame(struct page_table *pt, int frame) {
+	int page;
+	int currFrame;
+	int bits;
+	for (page = 0; page < page_table_get_npages(pt); page++) {
+		page_table_get_entry(pt, page, &currFrame, &bits);
+		if (currFrame == frame) {
+			return currFrame;
+		}
+	}
+	return -1;
 }
